1aacb2971afc6364cdd8a2dd07edacfbf9e2bace
Anton <anton415@gmail.com>
2017-11-06 12:55:39 +0300

    Correction of remarks.

--- 5. project_ Memory Game/js/app.js
+++ 5. project_ Memory Game/js/app.js
@@ -1,3 +1,76 @@
+/**
+ * The action can be 1 or 2. In order to determine which card is upside, first or second.
+ * @type {number}
+ */
+var action = 1;
+/**
+ *
+ */
+var firstCardData;
+/**
+ *
+ */
+var secondCardData;
+/**
+ *
+ */
+var firstCard;
+/**
+ *
+ */
+var secondCard;
+/**
+ *
+ * @type {number}
+ */
+var openCards = 0;
+/**
+ * Time spent on the game.
+ * @type {number}
+ */
+var time = performance.now();
+/**
+ * Number of moves.
+ * @type {number}
+ */
+var moveCount = 0;
+/**
+ * Number of moves. Used to reduce the number of stars.
+ * @type {number}
+ */
+var moveCountForRemoveStars = 0;
+/**
+ * Number of stars. Initially equal to 3. The more moves, the fewer stars.
+ * @type {number}
+ */
+var stars = 3;
+
+/**
+ * Click on card.
+ */
+$('.card').on("click", viewCard);
+
+/**
+ * Click on restart button.
+ */
+$('.restart').on("click", restart);
+
+/**
+ * Creation of an array of cards, for their shuffling.
+ * @type {jQuery|HTMLElement}
+ */
+var arrayCards = $('.card');
+// Shuffle cards.
+arrayCards = shuffle(arrayCards);
+// Add to page.
+$(".deck").append(arrayCards);
+
+
+/**
+ * Function for shuffle cards.
+ * @param array
+ * @returns {*}
+ */
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -12,70 +85,99 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+/**
+ * For restart button. Reload window.
  */
-
-var action = 1;
-var firstCardData;
-var secondCardData;
-var firstCard;
-var secondCard;
-var openCards = 0;
-var time = performance.now();
-var moveCount = 0;
-var moveCountForRemoveStars = 0;
-var stars = 3;
-
-$('.card').on("click", viewCard);
-
-$('.restart').on("click", restart);
-
 function restart() {
     window.location.reload();
 }
 
+/**
+ * For click on card. Show this card.
+ */
 function viewCard() {
-    congratulation();
+    /**
+     * If this first card, wait when will be second action.
+     */
     if (action === 1) {
+
+        /**
+         * The countdown begins with the first click on the card.
+         */
+        if(moveCount === 0) {
+            var display = document.querySelector('#time');
+            startTimer(display);
+        }
+
+        /**
+         * Show image of card.
+         */
         $(event.target).toggleClass('match');
         firstCardData = $(event.target);
         firstCard = event.target.childNodes.item(1);
+        /**
+         * Next, the second card will be opened.
+         * @type {number}
+         */
         action = 2;
 
+        /**
+         * Action for second card.
+         */
     } else {
+        /**
+         * Increase in the number of moves. 1 move = 2 open cards.
+         */
         moveCount++;
         moveCountForRemoveStars++;
+        /**
+         * Show image of card.
+         */
         $(event.target).toggleClass('match');
         secondCardData = $(event.target);
         secondCard = event.target.childNodes.item(1);
+
+        /**
+         * If the cards match they remain upside.
+         */
         if (firstCard.isEqualNode(secondCard)) {
             $(firstCardData).toggleClass('match');
             $(secondCardData).toggleClass('match');
             $(firstCardData).toggleClass('open show');
             $(secondCardData).toggleClass('open show');
             openCards = openCards + 2;
+            /**
+             * If all the cards are reversed, a congratulation will appear.
+             */
             if (openCards === 16) {
                 congratulation();
             }
+            /**
+             * If the cards do not match, wait for 2 seconds and flip them back.
+             */
         } else {
             $('.card').off("click");
             setTimeout(wait, 2000);
         }
+        /**
+         * The user has to turn the first card.
+         * @type {number}
+         */
         action = 1;
+        /**
+         * Display the number of stars.
+         */
         starRating();
+        /**
+         * Display the number of moves.
+         */
         moveDisplay();
     }
 }
 
+/**
+ * Display the number of stars.
+ */
 var starRating = function() {
     if (stars > 0) {
         if (moveCountForRemoveStars >= 16) {
@@ -86,10 +188,16 @@ var starRating = function() {
     }
 };
 
+/**
+ * Display the number of moves.
+ */
 var moveDisplay = function () {
     $('.moves').text(moveCount)
 }
 
+/**
+ * User Congratulations.
+ */
 var congratulation = function() {
     document.getElementById('win').removeAttribute('style');
     time = performance.now() - time;
@@ -98,6 +206,9 @@ var congratulation = function() {
     $('.allTime').html('You got: ' + moveCount + ' moves, ' + parseInt(time/1000) + ' seconds and ' + stars + ' stars.');
 };
 
+/**
+ * Wait 2 seconds before turning the cards back. There is also a blocking of the opening of cards.
+ */
 var wait = function() {
     $(firstCardData).toggleClass('match');
     $(secondCardData).toggleClass('match');
@@ -108,13 +219,10 @@ var wait = function() {
     $('.card').on("click", viewCard);
 };
 
-var arrayCards = $('.card');
-// Shuffle cards.
-arrayCards = shuffle(arrayCards);
-// Add to page.
-$(".deck").append(arrayCards);
-
-// time counter.
+/**
+ * Reflection of time during the game of the user.
+ * @param display
+ */
 function startTimer(display) {
     var start = Date.now(),
         diff,
@@ -138,8 +246,3 @@ function startTimer(display) {
     timer();
     setInterval(timer, 1000);
 }
-
-window.onload = function () {
-    var display = document.querySelector('#time');
-    startTimer(display);
-};
